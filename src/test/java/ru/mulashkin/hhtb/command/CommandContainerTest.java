719cd04cc46aba1c507bf3b797c658371acb8752
FedorMulashkin <fedor.mulashkin@gmail.com>
2022-09-12 20:40:15 +0300

    HHTB-3_5: Add unit tests to service and available commands

---
+++ src/test/java/ru/mulashkin/hhtb/command/CommandContainerTest.java
@@ -0,0 +1,43 @@
+package ru.mulashkin.hhtb.command;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.mulashkin.hhtb.service.SendBotMessageService;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Unit-level testing for CommandContainer")
+class CommandContainerTest {
+
+    private CommandContainer commandContainer;
+
+    @BeforeEach
+    public void init() {
+        SendBotMessageService sendBotMessageService = Mockito.mock(SendBotMessageService.class);
+        commandContainer = new CommandContainer(sendBotMessageService);
+    }
+
+    @Test
+    public void shouldGetAllTheExistingCommands() {
+        Arrays.stream(CommandName.values()).forEach(commandName -> {
+            Command command = commandContainer.retrieveCommand(commandName.getCommandName());
+            Assertions.assertNotEquals(UnknownCommand.class, command.getClass());
+        });
+    }
+
+    @Test
+    public void shouldReturnUnknownCommand() {
+        String unknownCommand = "/asdfas";
+
+        Command command = commandContainer.retrieveCommand(unknownCommand);
+
+        Assertions.assertEquals(UnknownCommand.class, command.getClass());
+    }
+
+
+}
